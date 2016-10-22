374f347165a85af1eebd0e1768f630b6180ca511
Michael Lenzen <m.lenzen@gmail.com>
2016-10-22 09:53:56 -0500

    Refactor and simplify tests.

--- tests.py
+++ tests.py
@@ -8,29 +8,6 @@ from flask_url_for_helpers import url_for_obj, register_url_for_obj, url_update
 db = flask_sqlalchemy.SQLAlchemy()
 
 
-@pytest.fixture(scope='session')
-def app(request):
-	_app = flask.Flask(__name__)
-	_app.config.update({
-		'SERVER_NAME': 'localhost',
-		'TESTING': True,
-		'DEBUG': True,
-		})
-	db.init_app(_app)
-	ctx = _app.app_context()
-	ctx.push()
-
-	def fin():
-		ctx.pop()
-	request.addfinalizer(fin)
-	return _app
-
-
-@pytest.fixture(scope='session')
-def client(app):
-	return app.test_client()
-
-
 class Employee(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
@@ -43,24 +20,44 @@ class Manager(db.Model):
 	last_name = db.Column(db.String)
 
 
-def test_url_for_obj_simple(app, client):
+@pytest.fixture(scope='session')
+def app(request):
+	_app = flask.Flask(__name__)
+	_app.config.update({
+		'SERVER_NAME': 'localhost',
+		'TESTING': True,
+		'DEBUG': True,
+		})
+	db.init_app(_app)
+	ctx = _app.app_context()
+	ctx.push()
+	request.addfinalizer(lambda: ctx.pop())
 
 	@register_url_for_obj(Employee)
-	@app.route('/employee/<int:id>')
+	@_app.route('/employee/<int:id>')
 	def employee_view(id):
 		pass
 
+	@register_url_for_obj(Manager, {
+		'full_name': lambda manager: manager.first_name + '_' + manager.last_name,
+		})
+	@_app.route('/manager/<full_name>')
+	def manager_view(full_name):
+		pass
+
+	return _app
+
+
+@pytest.fixture(scope='session')
+def client(app):
+	return app.test_client()
+
+
+def test_url_for_obj_simple(client):
 	employee = Employee(id=1)
 	assert url_for_obj(employee) == flask.url_for('employee_view', id=1)
 
 
-def test_url_for_obj_mapped(app, client):
-	@register_url_for_obj(Manager, {
-		'full_name': lambda manager: manager.first_name + '_' + manager.last_name,
-		})
-	@app.route('/manager/<full_name>')
-	def manager_view(full_name):
-		pass
-
+def test_url_for_obj_mapped(client):
 	manager = Manager(first_name='M', last_name='Lenzen')
 	assert url_for_obj(manager) == flask.url_for('manager_view', full_name='M_Lenzen')
